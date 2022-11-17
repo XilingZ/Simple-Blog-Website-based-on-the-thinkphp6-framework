a0acd41bf10a4a677c2f05fda4c5bc21ece49947
Xiling <obazxl@gmail.com>
2022-11-16 17:56:04 -0800

    english and screenshots

--- app/controller/Admins.php
+++ app/controller/Admins.php
@@ -43,7 +43,7 @@ class Admins extends BaseController
             }
             else{
                 Session::set('admin_name',input('post.name'));
-                return '登录成功';
+                return 'Login successfully';
             }
         }
         
